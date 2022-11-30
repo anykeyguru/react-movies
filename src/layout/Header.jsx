c6706dfca279e8558780889d368151d280a4e207
Ernest <240509865e@gmail.com>
2022-11-30 09:18:58 +0500

    added link to gh

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 function Header() {
+    const url = "https://github.com/anykeyguru/react-movies";
     return (
         <nav className="amber darken-3">
             <div className="nav-wrapper">
-                <a href="http://localhost:3000/" className="brand-logo">React Movies:</a>
+                <a href={url} className="brand-logo">React Movies:</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
 
-                    <li><a href="http://localhost:3000/">GitRepo</a></li>
+                    <li><a href={url} >GitRepo</a></li>
                 </ul>
             </div>
         </nav>
